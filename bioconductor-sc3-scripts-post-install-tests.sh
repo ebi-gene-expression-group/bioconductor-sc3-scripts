4c5f45a2ecbbec6c5a5ff637727650ab87f5a520
nh3 <nh3@users.noreply.github.com>
2019-03-15 12:44:32 +0000

    Feature/update test for version bump (#20)
    * Change clusters output to a single file (#12)
    
    * Update test and travis for accommodating version bump

--- bioconductor-sc3-scripts-post-install-tests.sh
+++ bioconductor-sc3-scripts-post-install-tests.sh
@@ -101,6 +101,8 @@ export sc3_validation_done="$output_dir/validation_done.txt"
 ### Workflow parameters
 
 export col_names=TRUE
+export cell_metrics=total_counts,total_features_by_counts
+export gene_metrics=n_cells_by_counts
 export min_cell_total_counts=500
 export min_cell_total_features=50
 export min_feature_n_cells_counts=10
